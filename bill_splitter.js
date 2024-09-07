0fface07b419642c4ec015070536cc6832152cf9
Christian Schmidt <cjschmidt33@gmail.com>
2024-09-07 15:42:20 -0400

    Change "cc_2.js" to "bill_splitter.js" per assigment submission details.

--- bill_splitter.js
+++ bill_splitter.js
@@ -5,3 +5,6 @@ let tip = (50 <= x <= 300) ? *0.15 : *0.2;
 
 // "tip" will be multiplied by 0.15 IF "x" (bill) is in between 50 and 300, otherwise it will be multiplied by 0.20.
 
+
+// Task 2: Output Details
+
