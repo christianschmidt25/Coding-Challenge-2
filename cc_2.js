720759aa955d31711b666d9ccd8ebcc7ef4b4950
Christian Schmidt <cjschmidt33@gmail.com>
2024-09-07 15:37:24 -0400

    Initial logic for calculating tip using ternary operator

---
+++ cc_2.js
@@ -0,0 +1,7 @@
+// Task 1: Calculate the Tip
+
+const x = "bill";
+let tip = (50 <= x <= 300) ? *0.15 : *0.2;
+
+// "tip" will be multiplied by 0.15 IF "x" (bill) is in between 50 and 300, otherwise it will be multiplied by 0.20.
+
