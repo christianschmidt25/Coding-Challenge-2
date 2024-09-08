6b74b058bfee158f8b6a44969b1ad97e147c2dbc
Christian Schmidt <cjschmidt33@gmail.com>
2024-09-08 18:11:49 -0400

    Create and test calculateTip function

--- bill_splitter.js
+++ bill_splitter.js
@@ -1,6 +1,6 @@
 // Task 1: Calculate the Tip
 
-let x = 100.00; //bill amount (x) is $100
+let x = 200.00; //bill amount (x) is $100
 let tip = (50 <= x <= 300) ? x*0.15 : x*0.20;
 
 // tip will be multiplied by 0.15 IF bill is in between 50 and 300, otherwise it will be multiplied by 0.20.
@@ -12,3 +12,15 @@ let totalbill = x + tip;
 console.log(`Thank you for eating here. Your bill was $${x}, your tip was $${tip}, and your total bill was $${totalbill}`);
 
 //The console log will output the correct numbers since it is not in quotations.
+
+
+// Task 3: Create a Function (calculateTip)
+
+function calculateTip(testBill, tip) {
+    return (tip)
+};
+
+let testBill = 100;
+console.log("Tip amount when the bill is $100 is", calculateTip(100,15));
+
+//will return the tip amount (calculated by ternary operator in step 1) when this test bill is $100.
